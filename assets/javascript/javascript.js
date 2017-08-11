18a196ecad6e78c388c7e536190d0ebcfff01108
Jay Sullivan <JaySull514@gmail.com>
2017-08-11 16:19:20 -0500

    initial commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,17 @@
+$(window).scroll(function() {
+    if ($(this).scrollTop() < 460) {
+        $('.nav-icon').css({
+            'opacity': 0
+        });
+    } else if ($(this).scrollTop() > 460) {
+        $('.nav-icon').css({
+            'opacity': -5 + ($(this).scrollTop() / 100)
+        });
+    };
+});
+
+console.log('hello');
+
+$(document).on('click', '.contact-submit', function() {
+    event.preventDefault();
+})
