38c04fea7ad1083bc377ea5da64eb75606a705e2
Jay Sullivan <JaySull514@gmail.com>
2017-11-02 10:02:48 -0500

    Added link to and picture of store example

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,21 +1,18 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() < 460) {
-        $('.nav-icon').css({
-            'opacity': 0
-        });
-    } else if ($(this).scrollTop() > 460) {
-        $('.nav-icon').css({
-            'opacity': -4 + ($(this).scrollTop() / 110)
-        });
-    };
+$(window).scroll(function () {
+  if ($(this).scrollTop() < 460) {
+    $('.nav-icon').css({
+      opacity: 0,
+    });
+  } else if ($(this).scrollTop() > 460) {
+    $('.nav-icon').css({
+      opacity: -4 + ($(this).scrollTop() / 110),
+    });
+  }
 });
 
-console.log('hello');
-
-$(document).on('click', '.contact-submit', function() {
-    event.preventDefault();
-    $('.contact-name').val('');
-    $('.contact-email').val('');
-    $('.contact-message').val('');
-})
-
+$(document).on('click', '.contact-submit', () => {
+  event.preventDefault();
+  $('.contact-name').val('');
+  $('.contact-email').val('');
+  $('.contact-message').val('');
+});
