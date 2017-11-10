038cb6d89a9abaf3d74805e52fcb737f0574078b
Jay Sullivan <JaySull514@gmail.com>
2017-11-09 20:43:11 -0600

    Moved function to javsascript

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -12,6 +12,21 @@ $(window).scroll(function () {
 
 $(document).on('click', '.contact-submit', () => {
   event.preventDefault();
+
+  $.ajax({
+    url: "https://formspree.io/JaySull514@gmail.com",
+    method: "POST",
+    data: {
+      name: $('.contact-name').val(),
+      email: $('.contact-email').val(),
+      message: $('.contact-message').val(),
+          },
+    dataType: "json",
+    success: function(data){
+      console.log(data);
+      alert("Message has been sent, thanks");
+    }
+  });
   $('.contact-name').val('');
   $('.contact-email').val('');
   $('.contact-message').val('');
